43726be66dc1b1d302524431f5a212477970c7e7
Vincent <vincent@prismaict.nl>
2018-06-26 20:51:16 +0200

    adjusted the connect object

--- uport_tutorial.js
+++ uport_tutorial.js
@@ -4,13 +4,13 @@ const SimpleSigner = window.uportconnect.SimpleSigner
 const Connect = window.uportconnect.Connect
 const appName = 'UportTutorial'
 
-const connect = new Connect('Amsterdam Identiteit Balie', {
-  clientId: '2op3XWF4dpPYuyvFw5UpoQviVRs8wihzHZj',
-      network: 'rinkeby',
-      signer: SimpleSigner('a0e388225b15ea1fbb0a86547971abc7556049b6d22dc073b7744403e7058e39')
-  })
-const web3 = connect.getWeb3()
+const connect = new Connect('Marc\'s App voor WIGO4IT', {
+  clientId: '2on6y285bcCdwFtuYipqoLTN2d95YrhW2Cc',
+  network: 'rinkeby',
+  signer: SimpleSigner('8277c714ab4e03d0c082004ab819c37495234cce74acd8169713c4bf1c927b07')
+ });
 
+const web3 = connect.getWeb3()
 
 // Setup the simple Status contract - allows you to set and read a status string
 const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
@@ -63,4 +63,14 @@ const setStatus = () => {
       globalState.txHashSetStatus = txHash
       render()
       })
+}
+
+// attest User
+const attest = () => {
+  var PROPERTY_VALUE = 'Lorem ipsum dolor sit amet';
+  console.log(connect);
+  connect.attestCredentials({
+    sub: connect.clientId,
+    claim: { 'claim': PROPERTY_VALUE },
+  });
 }
